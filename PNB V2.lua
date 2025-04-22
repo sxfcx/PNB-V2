71558c9eb56231de75cc66c0d6fa5e9550d1d132
sxfcx <valtteri.kunttu@gmail.com>
2025-04-22 18:23:52 +0300

    Update PNB V2.lua

--- PNB V2.lua
+++ PNB V2.lua
@@ -3,7 +3,7 @@ return {
   801003, -- SXFCC
   458552, -- Drians
   373174, -- Drians ADD UID
-  38405, -- DLAST
+  929149, -- DLAST
   827167, -- Itermitas
   926888, -- itermitas add uid
   112290, -- Kodokterbang
